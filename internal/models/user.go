1d8b10cd1c2c5e3eb26990f4a1be29188fc6f910
Ryan <nguyenduylan711@gmail.com>
2025-01-17 17:16:38 +0700

    chore: add user utils and hooks

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "eskept/internal/constants"
+import (
+	"eskept/internal/constants"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 const (
 	UserTableName = "users"
@@ -11,10 +16,32 @@ type User struct {
 	DeletedAt int64                `json:"deleted_at"`
 	Status    constants.UserStatus `json:"status" gorm:"default:pending_activation"`
 	UserRoles constants.UserRoles  `json:"role" gorm:"default:user_guest"`
-	Username  string               `json:"username"`
+	Email     string               `json:"email"`
 	Password  string               `json:"password"`
 }
 
 func (u *User) TableName() string {
 	return UserTableName
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	return u.HashPassword(u.Password)
+}
+
+func (u *User) BeforeUpdate(tx *gorm.DB) error {
+	return u.HashPassword(u.Password)
+}
+
+func (u *User) HashPassword(password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hash)
+	return nil
+}
+
+func (u *User) ComparePassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)) == nil
 }
