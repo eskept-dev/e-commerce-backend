22bf9c3244dde51cd9de7bc5582b0508bbbde1f8
Ryan <nguyenduylan711@gmail.com>
2025-01-24 15:57:04 +0700

    chore: add tokenize utils

--- internal/utils/stringUtils.go
+++ internal/utils/stringUtils.go
@@ -3,5 +3,14 @@ package utils
 import "strings"
 
 func ToSnakeCase(value string) string {
-	return strings.ToLower(strings.ReplaceAll(value, " ", "_"))
+	toRemoveSeparators := []string{" ", ",", ".", "+", "-", "/", "\\", "@", "#", "$", "%", "^", "&", "*"}
+	for _, sep := range toRemoveSeparators {
+		value = strings.ReplaceAll(value, sep, "_")
+	}
+	return strings.ToLower(value)
+}
+
+func Tokenize(value string) []string {
+	snakeValue := ToSnakeCase(value)
+	return strings.Split(snakeValue, "_")
 }
